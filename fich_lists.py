82441fcbcf3999962a91dda97e9d9cc9dbb7ee5a
Sergio Cobos Manjavacas <scobos@gamma16.aulas.gsyc.urjc.es>
2017-01-31 12:29:22 +0100

    Ejercicio 13.4

--- fich_lists.py
+++ fich_lists.py
@@ -4,5 +4,5 @@ fich= open("/etc/passwd","r")
 lineas= fich.readlines()
 for linea in lineas:
 	token = linea.split(':')
-	print("User: " , token[0] , ". Shell que utiliza: ", token [-1])
+	print("User: " , token[0] , " Shell que utiliza: ", token [-1][:-1])
 fich.close()
